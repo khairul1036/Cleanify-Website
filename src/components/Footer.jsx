42ed4d4b47469493b993c1afc3f8a8e7f9133d02
Khairul Islam <khairul15-4102@diu.edu.bd>
2024-12-26 01:51:05 +0600

    design footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,80 +1,73 @@
-import React from 'react';
+import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="pt-12 pb-6 px-10 font-[sans-serif] tracking-wide">
-      <div className="max-w-screen-xl mx-auto">
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
-          <div className="lg:flex lg:items-center">
-            <a href="javascript:void(0)">
-              <img src="https://readymadeui.com/readymadeui-light.svg" alt="logo" className="w-48" />
+    <footer className="bg-blue-600 text-white p-10">
+      <div className="footer max-w-screen-2xl mx-auto">
+        <aside>
+          <h1 className="text-5xl font-bold text-white">Cleanify</h1>
+          <p className="text-lg md:w-[400px]">
+          Cleanify is a website dedicated to providing expert cleaning services, offering convenient scheduling, eco-friendly solutions, and high-quality results for homes and businesses.
+          </p>
+        </aside>
+        <nav>
+          <h6 className="text-2xl font-bold text-white">Services</h6>
+          <a className="link link-hover text-lg">Branding</a>
+          <a className="link link-hover text-lg">Design</a>
+          <a className="link link-hover text-lg">Marketing</a>
+          <a className="link link-hover text-lg">Advertisement</a>
+        </nav>
+        <nav>
+          <h6 className="text-2xl font-bold text-white">Company</h6>
+          <a className="link link-hover text-lg">About us</a>
+          <a className="link link-hover text-lg">Contact</a>
+          <a className="link link-hover text-lg">Jobs</a>
+          <a className="link link-hover text-lg">Press kit</a>
+        </nav>
+        <nav>
+          <h6 className="text-2xl font-bold text-white">Social</h6>
+          <div className="grid grid-flow-col gap-4">
+            <a>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                className="fill-current"
+              >
+                <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path>
+              </svg>
+            </a>
+            <a>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                className="fill-current"
+              >
+                <path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path>
+              </svg>
+            </a>
+            <a>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                className="fill-current"
+              >
+                <path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path>
+              </svg>
             </a>
           </div>
-
-          <div className="lg:flex lg:items-center">
-            <ul className="flex space-x-6">
-              <li>
-                <a href="javascript:void(0)">
-                  <svg xmlns="http://www.w3.org/2000/svg" className="fill-gray-300fill-white w-7 h-7" viewBox="0 0 24 24">
-                    <path fillRule="evenodd" d="M19 3H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h7v-7h-2v-3h2V8.5A3.5 3.5 0 0 1 15.5 5H18v3h-2a1 1 0 0 0-1 1v2h3v3h-3v7h4a2 2 0 0 0 2-2V5a2 2 0 0 0-2-2z" clipRule="evenodd" />
-                  </svg>
-                </a>
-              </li>
-              <li>
-                <a href="javascript:void(0)">
-                  <svg xmlns="http://www.w3.org/2000/svg" className="fill-gray-300fill-white w-7 h-7" viewBox="0 0 24 24">
-                    <path fillRule="evenodd" d="M21 5a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V5zm-2.5 8.2v5.3h-2.79v-4.93a1.4 1.4 0 0 0-1.4-1.4c-.77 0-1.39.63-1.39 1.4v4.93h-2.79v-8.37h2.79v1.11c.48-.78 1.47-1.3 2.32-1.3 1.8 0 3.26 1.46 3.26 3.26zM6.88 8.56a1.686 1.686 0 0 0 0-3.37 1.69 1.69 0 0 0-1.69 1.69c0 .93.76 1.68 1.69 1.68zm1.39 1.57v8.37H5.5v-8.37h2.77z" clipRule="evenodd" />
-                  </svg>
-                </a>
-              </li>
-              <li>
-                <a href="javascript:void(0)">
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" className="fill-gray-300fill-white w-7 h-7" viewBox="0 0 24 24">
-                    <path d="M22.92 6c-.77.35-1.6.58-2.46.69.88-.53 1.56-1.37 1.88-2.38-.83.5-1.75.85-2.72 1.05C18.83 4.5 17.72 4 16.46 4c-2.35 0-4.27 1.92-4.27 4.29 0 .34.04.67.11.98-3.56-.18-6.73-1.89-8.84-4.48-.37.63-.58 1.37-.58 2.15 0 1.49.75 2.81 1.91 3.56-.71 0-1.37-.2-1.95-.5v.03c0 2.08 1.48 3.82 3.44 4.21a4.22 4.22 0 0 1-1.93.07 4.28 4.28 0 0 0 4 2.98 8.521 8.521 0 0 1-5.33 1.84c-.34 0-.68-.02-1.02-.06C3.9 20.29 6.16 21 8.58 21c7.88 0 12.21-6.54 12.21-12.21 0-.19 0-.37-.01-.56.84-.6 1.56-1.36 2.14-2.23z" />
-                  </svg>
-                </a>
-              </li>
-            </ul>
-          </div>
-
-          <div>
-            <h4 className="text-lg mb-6 dark:text-red-600">Useful links</h4>
-            <ul className="space-y-4 pl-2">
-              <li>
-                <a href="javascript:void(0)" className="text-gray-600 text-sm">Featured</a>
-              </li>
-              <li>
-                <a href="javascript:void(0)" className="text-gray-600 text-sm">New Arrivals</a>
-              </li>
-              <li>
-                <a href="javascript:void(0)" className="text-gray-600 text-sm">New Arrivals</a>
-              </li>
-            </ul>
-          </div>
-
-          <div>
-            <h4 className="text-lg mb-6 ">Information</h4>
-            <ul className="space-y-4 pl-2">
-              <li>
-                <a href="javascript:void(0)" className="text-gray-600 text-sm">About Us</a>
-              </li>
-              <li>
-                <a href="javascript:void(0)" className="text-gray-600 text-sm">Terms &amp; Conditions</a>
-              </li>
-              <li>
-                <a href="javascript:void(0)" className="text-gray-600 text-sm">Privacy Policy</a>
-              </li>
-              <li>
-                <a href="javascript:void(0)" className="text-gray-600 text-sm">Sale</a>
-              </li>
-              <li>
-                <a href="javascript:void(0)" className="text-gray-600 text-sm">Documentation</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-
-        <p className="text-gray-600 text-sm mt-10">© ReadymadeUI. All rights reserved.</p>
+        </nav>
+      </div>
+      <div className="pt-5">
+        <p className="text-center border-t border-gray-200 pt-2">
+          Copyright © {new Date().getFullYear()} - All right reserved by ACME
+          Industries Ltd
+        </p>
       </div>
     </footer>
   );
